198fe95850965ee9857b3259cb6e68bd0cb8b3da
Sriram K <sriram.k2@tcs.com>
2016-12-08 11:22:18 +0530

    Creating the generator script

--- lib/generators/task_at_tick_generator.rb
+++ lib/generators/task_at_tick_generator.rb
@@ -1,23 +1,24 @@
-module TaskAtTick
-  class InstallGenerator < Rails::Generators::Base
-    def create_initializer_file
-      include Rails::Generators::Migration
+require 'rails/generators/base'
+require 'rails/generators/migration'
+require 'rails/generators/active_record'
+require 'generators/next_migration_version'
 
-      source_root File.expand_path('../templates', __FILE__)
 
-      def copy_migrations
-        copy_migration 'create_tasks'
-      end
+class TaskAtTickGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+  extend NextMigrationVersion
 
-      protected
-        def copy_migration file_name
-          if self.class.migration_exists?("db/migrate", "#{file_name}")
-            say_status("skipped", "Migration #{file_name}.rb already exists")
-          else
-            migration_template "migrations/#{file_name}.rb", "db/migrate/#{file_name}.rb"
-          end
-        end
-      end
+  source_paths << File.join(File.dirname(__FILE__), 'templates')
+
+  def create_migration_file
+    if self.class.migration_exists?('db/migrate', 'create_tasks')
+      say_status('skipped', 'Migration CreateTasks already exists')
+    else
+      migration_template 'create_tasks.rb', 'db/migrate/create_tasks.rb'
     end
+  end
+
+  def self.next_migration_number(dirname)
+    ActiveRecord::Generators::Base.next_migration_number dirname
   end
 end
